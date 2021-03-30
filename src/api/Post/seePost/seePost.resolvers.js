d4f690eb7a212b8d92f3c692c2777bc3da8741eb
leeon <leeonechang92@gmail.com>
2021-03-30 16:11:25 +0900

    who likes the post?

--- src/api/Post/seePost/seePost.resolvers.js
+++ src/api/Post/seePost/seePost.resolvers.js
@@ -2,27 +2,14 @@ import prisma from "../../../util/prisma";
 
 export default {
   Query: {
-    whoLikesPost: async (_, args) => {
-      const { postId, limit, offset } = args;
-      let likes;
-      if (offset) {
-        likes = await prisma.post
-          .findUnique({
-            where: {
-              id: postId,
-            },
-          })
-          .likes({ take: limit, skip: offset });
-      } else {
-        likes = await prisma.post
-          .findUnique({
-            where: {
-              id: postId,
-            },
-          })
-          .likes({ take: limit });
-      }
-      return likes;
+    seePost: async (_, args) => {
+      const { postId } = args;
+      const post = await prisma.post.findUnique({
+        where: {
+          id: postId,
+        },
+      });
+      return post;
     },
   },
 };
--- src/api/Post/whoLikesPost/whoLikesPost.resolvers.js
+++ src/api/Post/whoLikesPost/whoLikesPost.resolvers.js
@@ -2,14 +2,27 @@ import prisma from "../../../util/prisma";
 
 export default {
   Query: {
-    seePost: async (_, args) => {
-      const { postId } = args;
-      const post = await prisma.post.findUnique({
-        where: {
-          id: postId,
-        },
-      });
-      return post;
+    whoLikesPost: async (_, args) => {
+      const { postId, limit, offset } = args;
+      let likes;
+      if (offset) {
+        likes = await prisma.post
+          .findUnique({
+            where: {
+              id: postId,
+            },
+          })
+          .likes({ take: limit, skip: offset });
+      } else {
+        likes = await prisma.post
+          .findUnique({
+            where: {
+              id: postId,
+            },
+          })
+          .likes({ take: limit });
+      }
+      return likes;
     },
   },
 };
